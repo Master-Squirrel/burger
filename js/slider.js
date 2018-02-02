4021574ed6b8546129e67f3d402bad7e2ab19ea5
Nikolay Yakushev <webcaster@mail.ru>
2018-02-03 00:41:27 +0300

    Insane #9

--- js/slider.js
+++ js/slider.js
@@ -1,15 +1,49 @@
         var leftArrow = document.querySelector('.slider__links-left');
-        var burgerSlide = document.querySelector('.slider__item');
+        var rightArrow = document.querySelector('.slider__links-right');
+        var burgerSlide = document.querySelector('.slider__list');
+        var start = 1;
 
         leftArrow.addEventListener('click', function (e) {
             e.preventDefault();
             var currentLeft = (parseInt(getComputedStyle(burgerSlide).left));
 
-            if (!currentLeft) {
-                currentLeft = 600;
-                console.log(currentLeft);
-            }
+     
 
-            burgerSlide.style.left = currentLeft + 'px';
-            console.log(getComputedStyle(burgerSlide).left);
+            if (start > 1) {
+
+                burgerSlide.style.left = currentLeft + 1100 + 'px';
+                start--;
+            } else {
+                burgerSlide.style.left = currentLeft - 4*1100 + 'px';
+                start = 5;
+            }
+            
+
+
+
+
+        })
+
+        rightArrow.addEventListener('click', function (e) {
+            e.preventDefault();
+            var currentLeft = (parseInt(getComputedStyle(burgerSlide).left));
+
+
+            
+        
+
+            if (start < 5) {
+
+                burgerSlide.style.left = currentLeft - 1100 + 'px';
+                start++;
+            } else {
+                burgerSlide.style.left = 0 + 'px';
+                start = 1;
+            }
+            
+
+
+
+
+
         })

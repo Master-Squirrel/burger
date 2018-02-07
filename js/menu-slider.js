94661c94606a18bcfa9a7e89d0e2c1fd1c99bec0
Nikolay Yakushev <nikolay.yakushev@Filuet.local>
2018-02-07 15:53:01 +0300

    Slider Fix

--- js/menu-slider.js
+++ js/menu-slider.js
@@ -1,55 +1,84 @@
+//Vertical Acco
 var allSliderItems = document.querySelectorAll(".accordion__item");
-var AllSliderHorizontItems = document.querySelectorAll(".team__item");
-
 
 function hideAllMenuTabs() {
 
     allSliderItems.forEach(function (item) {
-        console.log(item);
-        item.classList.remove('accordion__item--active');
-        item.children[1].classList.add('visually-hidden');
-      })
+
+        item
+            .classList
+            .remove('accordion__item--active');
+        item
+            .children[1]
+            .classList
+            .add('visually-hidden');
+    })
 
 }
+
+allSliderItems.forEach(function (item) {
+
+    item.addEventListener('click', function (e) {
+        e.preventDefault();
+        hideAllMenuTabs();
+
+    })
+
+    item.addEventListener('click', function () {
+        item
+            .classList
+            .add('accordion__item--active');
+        item
+            .children[1]
+            .classList
+            .remove('visually-hidden');
+    })
+
+})
+
+//Horizontal Acco
+var AllSliderHorizontItems = document.querySelectorAll(".team__item");
 
 function hideAllMenuHorizontTabs() {
 
     AllSliderHorizontItems.forEach(function (e) {
-        
-        e.classList.remove('team__item--active');
-        
+
+        e
+            .classList
+            .remove('team__item--active');
+
     })
 
 }
 
-
-allSliderItems.forEach(function (item) {
-    
-    item.addEventListener('click', function(e) {
-        e.preventDefault();
-        hideAllMenuTabs();
-    
-       
-    })
-
-    item.addEventListener('click', function(){
-        item.classList.add('accordion__item--active');
-        item.children[1].classList.remove('visually-hidden');
-    })
-    
-      
-})
-
 AllSliderHorizontItems.forEach(function (e) {
-    
-    e.addEventListener('click', function(e) {
+
+    e.addEventListener('click', function (e) {
         e.preventDefault();
         hideAllMenuHorizontTabs();
-       
+
     })
 
-    e.addEventListener('click', function(){
-        e.classList.add('team__item--active');
+    e.addEventListener('click', function () {
+        e
+            .classList
+            .add('team__item--active');
     })
-      
+
 })
+
+//Slider Menu Button
+var ingrButton = document.querySelectorAll('.slider__menu');
+var ingrSubMenu = document.querySelectorAll('.slider__submenu');
+
+ingrButton.forEach(function(item) {
+
+    item.addEventListener('mouseover', function () {
+ 
+    item.nextElementSibling.style.display = "block";
+    })
+    item.addEventListener('mouseout', function () {
+      
+        item.nextElementSibling.style.display = "none";
+    })
+});
--- js/slider.js
+++ js/slider.js
@@ -1,18 +1,20 @@
 var leftArrow = document.querySelector('.slider__links-left');
 var rightArrow = document.querySelector('.slider__links-right');
 var burgerSlide = document.querySelector('.slider__list');
+var size = parseInt(window.getComputedStyle(document.querySelector('.slider-container')).width);
+
 var start = 1;
 
 leftArrow.addEventListener('click', function (e) {
     e.preventDefault();
     var currentLeft = (parseInt(getComputedStyle(burgerSlide).left));
 
-    if (start > 1 && currentLeft % 1100 == 0) {
+    if (start > 1 && currentLeft % size == 0) {
         console.log(currentLeft + ' ' +  5 % 2 )
-        burgerSlide.style.left = currentLeft + 1100 + 'px';
+        burgerSlide.style.left = currentLeft + size + 'px';
         start--;
-    } else if (currentLeft % 1100 == 0) {
-        burgerSlide.style.left = currentLeft - 4 * 1100 + 'px';
+    } else if (currentLeft % size == 0) {
+        burgerSlide.style.left = currentLeft - 4 * size + 'px';
         start = 5;
     }
 
@@ -22,13 +24,20 @@ rightArrow.addEventListener('click', function (e) {
     e.preventDefault();
     var currentLeft = (parseInt(getComputedStyle(burgerSlide).left));
 
-    if (start < 5 && currentLeft % 1100 == 0) {
+    if (start < 5 && currentLeft % size == 0) {
 
-        burgerSlide.style.left = currentLeft - 1100 + 'px';
+        burgerSlide.style.left = currentLeft - size + 'px';
         start++;
-    } else if (currentLeft % 1100 == 0) {
+    } else if (currentLeft % size == 0) {
         burgerSlide.style.left = 0 + 'px';
         start = 1;
     }
 
+})
+
+window.addEventListener('resize', function() {
+    size = parseInt(window.getComputedStyle(document.querySelector('.slider-container')).width);
+    console.log(size);
+    currentLeft = (parseInt(getComputedStyle(burgerSlide).left));
+    burgerSlide.style.left = -size*(start-1) + 'px';
 })
